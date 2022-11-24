a4ea7fe0ea6adbf0cdd4e4f33c09364b56617ebf
IliaNoah <vaguaman86@gmail.com>
2022-11-24 16:55:04 +0330

    added some comments and changes

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 "use strict";
 
+// random number creator variable
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
+// other variables
 let score = 20;
 let highScore = 0;
-console.log(secretNumber);
 
+//show message functions
 function showMassage(message) {
   document.querySelector(".message").textContent = message;
 }
@@ -17,9 +19,11 @@ function showHighScore(currentHighScore) {
   document.querySelector('.highscore').textContent = currentHighScore;
 }
 
+// click event functions
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
 
+  // win condition
   if (guess === secretNumber) {
     showMassage("😍 You got the number!");
     document.querySelector("body").style.backgroundColor = "#60b347";
@@ -27,18 +31,27 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".number").style.borderRadius = "30rem";
     document.querySelector('.number').textContent = secretNumber;
 
+    // high score condition
     if (score > highScore) {
       highScore = score;
       showHighScore(score);
     }
+
+    // blank input condition
   } else if (!guess) {
     showMassage("🎈 Please input your number!");
+
+    // not in range number condition
   } else if (guess < 0 || guess > 20) {
     showMassage("🎲 Put a number between 1 and 20!");
+
+    // high and guess condition
   } else if (score > 2) {
     showMassage(guess > secretNumber ? "🙄 TO HIGH" : "😔 TO LOW");
     score -= 2;
     showScore(score);
+
+    // lose condition
   } else {
     showMassage("🎃 U Lost!");
     document.querySelector("body").style.backgroundColor = "#ff1153";
@@ -46,10 +59,10 @@ document.querySelector(".check").addEventListener("click", function () {
   }
 });
 
+// reset functions
 document.querySelector(".again").addEventListener("click", function () {
   // secretNumber reset
   secretNumber = Math.trunc(Math.random() * 20) + 1;
-  console.log(secretNumber);
   // score reset
   score = 20;
   showScore(score);
@@ -63,6 +76,7 @@ document.querySelector(".again").addEventListener("click", function () {
   document.querySelector('.number').textContent = "?";
   document.querySelector('.guess').value = "";
 
+  // high score reset condition
   if (highScore >= 20) {
     score = 0;
     showHighScore('0');
