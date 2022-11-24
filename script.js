6bc424be08cc7a8d00584bde2e3c114f0ac8cf4f
IliaNoah <vaguaman86@gmail.com>
2022-11-24 16:43:38 +0330

    finished project

---
+++ script.js
@@ -0,0 +1,70 @@
+"use strict";
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+console.log(secretNumber);
+
+function showMassage(message) {
+  document.querySelector(".message").textContent = message;
+}
+
+function showScore(currentScore) {
+  document.querySelector(".score").textContent = currentScore;
+}
+
+function showHighScore(currentHighScore) {
+  document.querySelector('.highscore').textContent = currentHighScore;
+}
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+
+  if (guess === secretNumber) {
+    showMassage("😍 You got the number!");
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+    document.querySelector(".number").style.borderRadius = "30rem";
+    document.querySelector('.number').textContent = secretNumber;
+
+    if (score > highScore) {
+      highScore = score;
+      showHighScore(score);
+    }
+  } else if (!guess) {
+    showMassage("🎈 Please input your number!");
+  } else if (guess < 0 || guess > 20) {
+    showMassage("🎲 Put a number between 1 and 20!");
+  } else if (score > 2) {
+    showMassage(guess > secretNumber ? "🙄 TO HIGH" : "😔 TO LOW");
+    score -= 2;
+    showScore(score);
+  } else {
+    showMassage("🎃 U Lost!");
+    document.querySelector("body").style.backgroundColor = "#ff1153";
+    showScore("0");
+  }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  // secretNumber reset
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  console.log(secretNumber);
+  // score reset
+  score = 20;
+  showScore(score);
+  // message reset
+  showMassage('Start guessing...');
+  // background reset
+  document.querySelector('body').style.backgroundColor = '#222';
+  // style reset
+  document.querySelector(".number").style.width = "15rem";
+  document.querySelector(".number").style.borderRadius = "0";
+  document.querySelector('.number').textContent = "?";
+  document.querySelector('.guess').value = "";
+
+  if (highScore >= 20) {
+    score = 0;
+    showHighScore('0');
+  }
+});
